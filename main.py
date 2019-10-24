d5efb78c4a1299527668480b73613b9f474b81b3
Kateřina Hroníková <katacek@gmail.com>
2019-10-24 12:29:13 +0200

    db_connect1

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import os
-from flask import Flask
-from flask import render_template  # !Important
+from flask import Flask, g, request, url_for, render_template, redirect
+from flask import render_template
+from jinja2 import exceptions  
+import psycopg2
 
-app = Flask(__name__)
+app = Flask('seniore')
+
+def get_db():
+  
+  # https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-python
+    database_url = os.environ['DATABASE_URL']
+  #database_url = os.environ["DATABASE_URL"]
+    con = psycopg2.connect(database_url, sslmode='require')
+    g.db = con
+    return g.db
 
 
 @app.route('/')
@@ -20,6 +31,15 @@ def prehled():
 @app.route('/sluzby')
 def sluzby():
     return render_template ('sluzby.html')
+
+@app.route('/database')
+def database():
+  db_connection = get_db()
+  cursor = db_connection.execute('select first_name, surname from users limit 10')
+  entries = cur.fetchall()
+  for row in entries:
+    print("Name: " + row[1] + ", surname: " + row[2])
+
 
 
 if __name__ == '__main__':
