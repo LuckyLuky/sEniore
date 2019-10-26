dc6ce928d65415b378af575d37f632c047b3424b
Kateřina Hroníková <katacek@gmail.com>
2019-10-26 18:28:50 +0200

    chosing service, upload to db, showing overview

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
 from flask import Flask, g, request, url_for, render_template, redirect
 from flask import render_template
-from jinja2 import exceptions  
+from jinja2 import exceptions
 import psycopg2
 import configparser 
+from flask_wtf import FlaskForm
+from wtforms import Form, BooleanField, StringField, SelectField, IntegerField, widgets, validators
 
 app = Flask('seniore')
+
+app.config["SECRET_KEY"] = "super tajny klic"
 
 configParser = configparser.RawConfigParser()   
 configFilePath = r'config.txt'
@@ -32,18 +36,41 @@ def registrace():
 def prehled():
   db_connection = get_db()
   cursor = db_connection.cursor()
-  cursor.execute('select u.first_name as first_name, u.surname as surname, s.category as category, d.demand_offer as demand_offer, u.address as address from users u left join users_services us on us.id_users = u.id left join services s on s.id = us.id_services left join demand_offer d on d.id = us.id_demand_offer limit 10')
+  cursor.execute('select u.first_name as first_name, u.surname as surname, s.category as category, d.demand_offer as demand_offer, u.address as address from users u left join users_services us on us.id_users = u.id left join services s on s.id = us.id_services left join demand_offer d on d.id = us.id_demand_offer order by u.id desc limit 10')
   entries = cursor.fetchall()
   return render_template ('prehled.html', entries = entries)
 
-@app.route('/sluzby')
-def sluzby():
-    return render_template ('sluzby.html')
+@app.route('/sluzby', methods=['POST', 'GET'])
+def sluzby_upload():
+    if request.method == 'GET':
+        return render_template('sluzby.html')
+    elif request.method == 'POST':
+        kwargs = {
+            'first_name': request.form['first_name'],
+            'surname': request.form['surname'],
+            'email': request.form['email'],
+            'address': request.form['address'],
+            'telephone': request.form['telephone'],
+            'demand_offer': request.form['demand_offer'],
+            'category': request.form['category'],
+            'secret_key': request.form['SECRET_KEY'],
+            'submit_value': request.form['submit'],
+        }
+        db_connection = get_db()
+        cursor = db_connection.cursor()
+        cursor.execute('SELECT nextval(\'users_id_seq\')')
+        unique_number_users = cursor.fetchone()
+        cursor.execute('insert into users (id, first_name, surname, email, address, telephone) values (%s, %s, %s, %s, %s, %s)',
+         (unique_number_users, request.form['first_name'], request.form['surname'], request.form['email'], request.form['address'], request.form['telephone']))
+        cursor.execute('SELECT nextval(\'users_services_id_seq\')')
+        cursor.execute('insert into users_services (id_demand_offer, id_services, id_users) values (%s, %s, %s)',
+        (request.form['demand_offer'], request.form['category'], unique_number_users))
+        db_connection.commit()
+        return render_template('sluzby_success.html', **kwargs)
 
 @app.route('/succes')
 def success():
     return render_template ('success.html')
-
 
 @app.route('/add_name', methods=['POST'])
 def add_name():
