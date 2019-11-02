6d05be66b33eddb3185779fcda4d9240460a5dec
Zlata Kelembet <zlata.kelembet@gmail.com>
2019-11-02 09:34:38 +0100

    Changed_r_d

--- python_random_generation/r_d_table.py
+++ python_random_generation/r_d_table.py
@@ -5,10 +5,7 @@ input_file = open('user_table.csv', encoding = 'utf-8')
 user_table = [line.strip().split(';') for line in input_file]
 input_file.close()
 
-# Open table with demands/requests type
-input_file = open('r_d_data.csv', encoding = 'utf-8')
-r_d_data = [line.strip().split(';') for line in input_file]
-input_file.close()
+r_d_data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]              # List of service types IDs
 
 # Create a list of User ID
 users = []
@@ -17,24 +14,21 @@ for user in user_table:
 
 users = users[1:]
 
-# Request/Demand type list
-r_d_type = ['poptavka', 'nabidka']
 
 # Creation of Request/Demand table
 
 r_d_table = []
 
 for u in users:
-    r_d_number = random.randint(0, 9)                                       # Choose the number of R/Ds for this User
+    r_d_number = random.randint(0, 10)                                          # Choose the number of R/Ds for this User
     n = 0  
-    data = r_d_data                                                         # 'Fake' r_d_data table for the cycle
-    if r_d_number < 8:
-        while n < r_d_number:
-            r_d = random.randint(0, len(data)-1)                            # Random number to choose one of R/D options from 'fake' table data
-            types = random.randint(0, 1)                                    # Choose if it is a Demand or a Request  
-            r_d_table = r_d_table +[[u, r_d_type[types], data[r_d][1]]]     # Create new line in r_d_table list
-            data = data[0:r_d] + data[r_d+1:]                               # Exclude used R/D option from our 'fake' data table to avoid duplicates
-            n = n + 1                                           
+    data = r_d_data                                                             # 'Fake' r_d_data table for the cycle
+    while n < r_d_number:
+        r_d = random.randint(0, len(data)-1)                                    # Random number to choose one of R/D options from 'fake' table data
+        types = random.randint(1, 2)                                            # Choose if it is a Demand or a Request  
+        r_d_table = r_d_table +[[10000+n, u, types, data[r_d]]]              # Create new line in r_d_table list
+        data = data[0:r_d] + data[r_d+1:]                                       # Exclude used R/D option from our 'fake' data table to avoid duplicates
+        n = n + 1                                           
 
 # download data to new file / overwrite in existing file
 new_data = open ('r_d_table.csv', 'w', encoding = 'utf-8')
