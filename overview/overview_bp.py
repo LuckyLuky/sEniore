872ded0d008411177ef266a75e6b6eb375e2ea29
wSeniore <oodoow@gmail.com>
2019-12-25 13:43:01 +0100

    prehled filtrovani zobrazeni vice sluzeb v mape

--- overview/overview_bp.py
+++ overview/overview_bp.py
@@ -83,71 +83,109 @@ def prehled_filtr():
         return render_template("prehled_success.html", entries=vysledekselectu, sndmap=map)
 
 
-@blueprint.route("/prehled_all", methods=["GET"])
+@blueprint.route("/prehled_all", methods=["POST","GET"])
 def prehled_all():
 
-    
-    if request.method == "GET":
-        vysledekselectu = DBAccess.ExecuteSQL(
-            """
-        SELECT u.first_name, u.surname, s.category, d.demand_offer, u.town, us.id, u.latitude, u.longitude, u.id, u.info
-        FROM users u
-        LEFT JOIN users_services us on us.id_users = u.id
-        LEFT JOIN services s on s.id = us.id_services
-        LEFT JOIN demand_offer d on d.id = us.id_demand_offer
-        WHERE d.id = %s
-        ORDER BY us.id desc
-        """,
-            (2,)
-        )
-
-        if vysledekselectu is None:
-            vysledekselectu = []
-
-        dbUser = DBAccess.GetDBUserById(session['id_user'])
-
-        if len(vysledekselectu) == 0:
-            flash("Bohužel v systému nejsou zadány žádné služby.") 
-
-
-        # markery pro kazdeho vyhledaneho
-        markers = []
-        marker = {}
-        marker["icon"] = "http://maps.google.com/mapfiles/kml/pal2/icon10.png"
-        marker["lat"] = str(dbUser.latitude)
-        marker["lng"] =  str(dbUser.longitude)
-        marker["infobox"] = f'<b>{dbUser.first_name} </b><br>{dbUser.info}<img class=img_mapa src= {GetImageUrl(dbUser.id)} />'
-        markers.append(marker)
-
-        for user in vysledekselectu:
-            pictureUrl = GetImageUrl(user[8])
-            marker = {}
-            marker["icon"] = "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
-            marker["lat"] = str(user[6])
-            marker["lng"] =  str(user[7])
-            marker["infobox"] = f'<b>{user[0]} {user[1]}</b><br>{user[9]} <img class=img_mapa src= {pictureUrl} /><br>nabízená služba: {user[2]} <br><a href="/match?id={user[5]}">Kontaktovat</a>'
-            markers.append(marker)
-
-        map = Map(
-                    identifier="sndmap",
-                    style="height:100%;width:100%;margin:0;",
-                    lat=str(dbUser.latitude),
-                    lng=str(dbUser.longitude),
-                    markers=markers
-                    )  # get map, zoom on location of actual user, insert markers from select, ie users who provide specific required service
-
-
     services = DBAccess.ExecuteSQL("select * from services")
+    checked_services_id_list = []
+    for service in services:
+        checked_services_id_list.append(service[0])
+
     form = regFormBuilder(
         services
     )  # put all services to form, but I need to display it - by for cycle below
     form.checkBoxes.clear()
-
+    form.demandOffer.choices = [('2','nabídky'),('1','poptávky')]
+ 
     for index in form.checkBoxIndexes:
+        checkbox = getattr(form, "checkbox%d" % index)
+        if(request.method == 'GET'):
+            checkbox.data = True
         form.checkBoxes.append(
             getattr(form, "checkbox%d" % index)
-        )  # displaying checkboxes on website   
+        )  # displaying checkboxes on website
 
-    return render_template("prehled_all.html", entries=vysledekselectu, sndmap=map, form = form)
+    if form.validate_on_submit():
+        checked_services_id_list = []
+        for index in form.checkBoxIndexes:
+            checkbox = getattr(form, "checkbox%d" % index)
+            if checkbox.data:
+                checked_services_id_list.append(checkbox.id)
+        
+    result = DBAccess.ExecuteSQL(
+        '''
+        SELECT u.id, u.first_name, u.surname, u.info, d.demand_offer, u.latitude, u.longitude, us.id,s.category
+        FROM users u
+        LEFT JOIN users_services us on us.id_users = u.id
+        LEFT JOIN services s on s.id = us.id_services
+        LEFT JOIN demand_offer d on d.id = us.id_demand_offer
+        WHERE   d.id = %s AND
+                us.id_services in %s
+        ORDER BY u.id, us.id
+        ''',(int(form.demandOffer.data),tuple(checked_services_id_list))
+    )
+    
+    if result is None:
+        result = []
+        flash("Bohužel v systému nejsou zadány žádné služby  dle vašeho filtru.")
+
+    usersCatDict = {}
+    
+    for row in result:
+        key = list(row)
+        key = tuple(key[:7])
+        # key = tuple(list(row)[:7])
+        value = row[-2:]
+        if(key in usersCatDict):
+            usersCatDict[key].append(value)
+        else:
+            usersCatDict[key] = [value]
+
+    demandOfferText = 'nabízené'
+    if(int(form.demandOffer.data)==1):
+        demandOfferText = 'poptávané'
+
+
+
+    
+    
+    dbUser = DBAccess.GetDBUserById(session['id_user'])
+
+    # markery pro kazdeho vyhledaneho
+    markers = []
+    marker = {}
+    marker["icon"] = "http://maps.google.com/mapfiles/kml/pal2/icon10.png"
+    marker["lat"] = str(dbUser.latitude)
+    marker["lng"] =  str(dbUser.longitude)
+    marker["infobox"] = f'<b>{dbUser.first_name} </b><br>{dbUser.info}<img class=img_mapa src= {GetImageUrl(dbUser.id)} />'
+    markers.append(marker)
+
+    for user in usersCatDict.keys():
+        servicesHTML = '<ul>'
+        for service in usersCatDict[user]:
+            servicesHTML=servicesHTML+f'<li> {service[1]} <a href="/match?id={service[0]}">Kontaktovat</a></li>'
+        servicesHTML=servicesHTML+'</ul>'
+
+        pictureUrl = GetImageUrl(user[0])
+        marker = {}
+        marker["icon"] = "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+        marker["lat"] = str(user[5])
+        marker["lng"] =  str(user[6])
+        marker["infobox"] = f'''<b>{user[1]} {user[2]}</b><p>{user[3]}</p>\
+            <img class=img_mapa src= {pictureUrl} /><br>\
+            <p>{demandOfferText} služby: {servicesHTML}</p>'''
+        markers.append(marker)
+
+    map = Map(
+                identifier="sndmap",
+                style="height:100%;width:100%;margin:0;",
+                lat=str(dbUser.latitude),
+                lng=str(dbUser.longitude),
+                markers=markers
+                )  # get map, zoom on location of actual user, insert markers from select, ie users who provide specific required service
+
+    return render_template("prehled_all.html", sndmap=map, form = form)
+   
+    
 
 
