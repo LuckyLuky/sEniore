c34c8f4564330a9035fbfcde66cb4b5aec4907c6
oodoow <oodoow@gmail.com>
2020-03-14 22:49:54 +0100

    mail edit

--- login/login_bp.py
+++ login/login_bp.py
@@ -366,7 +366,7 @@ def user_confirmation(token):
         abort(403)
     dbUser = DBAccess.GetDBUserById(user_id)
     DBAccess.ExecuteUpdate('update users set level=1 where id=%s', (user_id,))
-    email_text = f'''<html> Dobrý den, <br> Váš účet byl ověřen a nyní se můžete <a href="https://app.seniore.org/login/">nalogovat </a>. <br> 
+    email_text = f'''<html> Dobrý den, <br> Váš účet byl ověřen a nyní se můžete <a href="https://app.seniore.org/login/">přihlásit </a>. <br> 
     Věnujte prosím chviličku instrukcím, jak aplikaci používat. <br> 
     1. Na mapce uvidíte svojí polohu. V blízkosti se zobrazí lidé, kteří mohou pomoci, nebo pomoc potřebují. <br>
     Je možné, že se ve Vaší lokalitě zatím nikdo nepřihlásil. Kontaktujte prosím kohokoliv, kdo by se mohl zapojit. <br>
